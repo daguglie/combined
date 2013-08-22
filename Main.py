2bf9713a5ada941b5d0230f0dd750ee9483f25ba
daguglie <guglielmo@comcast.net>
2013-08-21 23:47:51 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,35 @@
 #LCD Only
+
+def message1()
+  return "first"
+def message2()
+  return "second"
+def message3()
+  return "last"
+
+from time import sleep
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
+lcd = Adafruit_CharLCDPlate(busnum=1)
+choosenScreen = 3
+
+lcd.clear()
+lcd.message("Raspberry Pi\nOven Mitt!")
+sleep(2)
+
+while True:
+  if choosenScreen == 0
+    choosenScreen = 3 #avoid modulus of 0 problems
+  if lcd.buttonPressed(lcd.RIGHT)
+    choosenScreen++
+  else if lcd.buttonPressed(lcd.RIGHT)
+    choosenScreen--
+  choosenScreen = choosenScreen % 3
+  if (choosenScreen == 0)
+    displayMessage = message1()
+  else if (choosenScreen == 1)
+    displayMessage = message2()
+  else if (choosenScreen == 2)
+    displayMessage = message3()
+  lcd.clear()
+  lcd.message(displayMessage)
