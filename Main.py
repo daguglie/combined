2f3cd700c7f7f3a1da4cd8509556dc8949167497
daguglie <guglielmo@comcast.net>
2013-08-22 10:42:41 -0700

    Update Main.py
    debouncing experiment

--- Main.py
+++ Main.py
@@ -12,26 +12,28 @@ from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 lcd = Adafruit_CharLCDPlate(busnum=1)
 choosenScreen = 3
-
+lastButtonPress = time.time()
 lcd.clear()
 lcd.message("Raspberry Pi\nOven Mitt!")
 sleep(2)
 
 while True:
-  if choosenScreen == 0:
-    choosenScreen = 3 #avoid modulus of 0 problems
-  if lcd.buttonPressed(lcd.RIGHT):
-    choosenScreen+=1
-  elif lcd.buttonPressed(lcd.LEFT):
-    choosenScreen-=1
-  choosenScreen = choosenScreen % 3
-  if (choosenScreen == 0):
-    displayMessage = message1()
-    lcd.clear()
-  elif (choosenScreen == 1):
-    displayMessage = message2()
-    lcd.clear()
-  elif (choosenScreen == 2):
-    displayMessage = message3()
-    lcd.clear()
-  lcd.message(displayMessage)
+  if (time.time() - lastButtonPress) > 2: #trying to debounce the buttons 
+    if choosenScreen == 0:
+      choosenScreen = 3 #avoid modulus of 0 problems
+    if lcd.buttonPressed(lcd.RIGHT):
+      choosenScreen+=1
+    elif lcd.buttonPressed(lcd.LEFT):
+      choosenScreen-=1
+    choosenScreen = choosenScreen % 3
+    if (choosenScreen == 0):
+      displayMessage = message1()
+      lcd.clear()
+    elif (choosenScreen == 1):
+      displayMessage = message2()
+      lcd.clear()
+    elif (choosenScreen == 2):
+      displayMessage = message3()
+      lcd.clear()
+    lcd.message(displayMessage)
+    lastButtonPress = time.time()
