fba0cd2a48a7c6543b593ec7b0bf3811b8be1e16
daguglie <guglielmo@comcast.net>
2013-08-22 11:21:39 -0700

    Create TempMod.py
    creating temperature module

---
+++ TempMod.py
@@ -0,0 +1,30 @@
+#Temperature Module
+
+import os
+import glob
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def readRawData
+  f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+    
+def getTemp():
+  lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+      time.sleep(0.2)
+      lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+      tempString = lines[1][equals_pos+2:]
+      tempC = float(tempString)/1000.0
+      tempF = tempC * 9.0 / 5.0 + 32.0
+      return tempC, tempF
