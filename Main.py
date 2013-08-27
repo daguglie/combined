04d826c20271a182b0aefc5e206f6e990a8b16e0
daguglie <guglielmo@comcast.net>
2013-08-26 22:17:42 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -102,6 +102,7 @@ def beat(channel):
     heartQueue.append(currentTime - prevTime)
     prevTime = currentTime
     beatTotalTime += heartQueue[-1]
+    print (heartQueue[-1] + '\n')
     if (len(heartQueue) > 10):
       beatTotalTime -= heartQueue[0]
       heartQueue.popleft()
